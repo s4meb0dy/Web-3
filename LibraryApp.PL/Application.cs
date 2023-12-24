4dd38a7b28917f1ab736add2addcdaaac99cc06d
s4meb0dy <96389216+s4meb0dy@users.noreply.github.com>
2023-12-24 15:27:48 +0200

    Add PL(console)

---
+++ LibraryApp.PL/Application.cs
@@ -0,0 +1,273 @@
+﻿using LibraryApp.BLL.DTOs;
+using LibraryApp.BLL.Interfaces;
+
+namespace LibraryApp.PL
+{
+    public class Application
+    {
+        private readonly IReaderService readerService;
+        private readonly IBookService bookService;
+        private readonly IFormService formService;
+        private readonly ILibraryService libraryService;
+        private readonly ICatalogService catalogService;
+
+        public Application(IReaderService readerService, IBookService bookService, IFormService formService, ILibraryService libraryService, ICatalogService catalogService)
+        {
+            this.readerService = readerService;
+            this.bookService = bookService;
+            this.formService = formService;
+            this.libraryService = libraryService;
+            this.catalogService = catalogService;
+        }
+
+        public async Task Start()
+        {
+            var option = RenderMenu();
+
+            var optionIsIntType = Int32.TryParse(option, out var value);
+
+            if (optionIsIntType)
+            {
+                await DefineSolveStrategy(value);
+            }
+
+            Console.WriteLine("\nERROR: Incorrect option selected!");
+            Thread.Sleep(2500);
+
+            await this.Start();
+        }
+
+        private string RenderMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Select option");
+            Console.WriteLine("<1> Add new READER");
+            Console.WriteLine("<2> Add new BOOK");
+            Console.WriteLine("<3> Add new FORM");
+            Console.WriteLine("<4> Get all READERS");
+            Console.WriteLine("<5> Get all BOOKS");
+            Console.WriteLine("<6> Get all FORMS");
+            Console.WriteLine("<7> Borrow");
+            Console.WriteLine("<8> Return");
+            Console.WriteLine("<9> Search");
+            Console.WriteLine("<0> Exit");
+            Console.Write("[YOUR CHOICE] Option: ");
+            
+            var option = Console.ReadLine();
+
+            return option;
+        }
+
+        private async Task DefineSolveStrategy(int option)
+        {
+            switch (option)
+            {
+                case 1:
+                    await AddReader();
+
+                    break;
+                case 2:
+                    await AddBook();
+
+                    break;
+                case 3:
+                    await AddFormManually();
+
+                    break;
+                case 4:
+                    await GetAllReaders();
+
+                    break;
+                case 5:
+                    await GetAllBooks();
+
+                    break;
+                case 6:
+                    await GetAllForms();
+
+                    break;
+                case 7:
+                    await Borrow();
+
+                    break;
+                case 8:
+                    await ReturnBook();
+
+                    break;
+                case 9:
+                    await SearchByCriterias();
+
+                    break;
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    break;
+            }
+
+            Console.WriteLine("\n<---SUCCESS--->");
+            Thread.Sleep(2500);
+
+            await this.Start();
+        }
+
+        private async Task SearchByCriterias()
+        {
+            Console.Clear();
+            Console.WriteLine("<-Search->");
+            Console.Write("Criteria 1 (or enter to ignore) ---- title: ");
+            var titleToSearch = Console.ReadLine();
+
+            Console.Write("Criteria 2 (or enter to ignore) ---- author: ");
+            var authorToSearch = Console.ReadLine();
+
+            Console.Write("Criteria 3 (or enter to ignore) ---- year: ");
+            var yearToSearch = Console.ReadLine();
+
+            var filteredBooks = await this.catalogService.SearchBooksAsync(
+                string.IsNullOrWhiteSpace(titleToSearch) ? null : titleToSearch,
+                string.IsNullOrWhiteSpace(authorToSearch) ? null : authorToSearch,
+                string.IsNullOrWhiteSpace(yearToSearch) ? null : int.Parse(yearToSearch));
+
+            foreach (var book in filteredBooks)
+            {
+                Console.WriteLine($"\nID = {book.Id}\nTitle = {book.Title}\nAuthor = {book.Author}\nYear = {book.Year}\nIs Available = {book.IsAvailable}\n");
+            }
+        }
+
+        private async Task ReturnBook()
+        {
+            Console.Clear();
+            Console.WriteLine("<-Return->");
+            Console.Write("Book ID to return = ");
+            var bookId = int.Parse(Console.ReadLine());
+
+            await this.libraryService.ReturnBookAsync(bookId);
+        }
+
+        private async Task Borrow()
+        {
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("<-Borrow->");
+                Console.Write("Reader ID = ");
+                var readerId = int.Parse(Console.ReadLine());
+
+                Console.Write("Book IDs (space between digits) = ");
+                var booksId = Console.ReadLine().Split(" ");
+                var intBooksIds = booksId.Select(x => int.Parse(x)).ToList();
+
+                await this.libraryService.BorrowBooksAsync(readerId, intBooksIds);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"\nError: {ex.Message}");
+            }
+        }
+
+        private async Task GetAllForms()
+        {
+            Console.Clear();
+            Console.WriteLine("<-All FORMS->");
+
+            var forms = await this.formService.GetAllFormsAsync();
+
+            foreach (var form in forms)
+            {
+                Console.WriteLine($"ID = {form.Id}\nReader ID = {form.ReaderId}\nReader full name = {form.Reader.FullName}\nBook ID = {form.BookId}\nBook title = {form.Book.Title}");
+            }
+        }
+
+        private async Task GetAllBooks()
+        {
+            Console.Clear();
+            Console.WriteLine("<-All BOOKS->");
+
+            var books = await this.bookService.GetAllBooksAsync();
+
+            foreach (var book in books)
+            {
+                Console.WriteLine($"ID = {book.Id}\nTitle = {book.Title}\nAuthor = {book.Author}\nYear = {book.Year}\nIs Available = {book.IsAvailable}\n");
+            }
+        }
+
+        private async Task GetAllReaders()
+        {
+            Console.Clear();
+            Console.WriteLine("<-All READERS->");
+
+            var readers = await this.readerService.GetAllReadersAsync();
+
+            foreach (var reader in readers)
+            {
+                Console.WriteLine($"ID = {reader.Id}\nFull name = {reader.FullName}\nAddress = {reader.Address}\n");
+            }
+
+            Console.Write("\nEnter any symbol to main menu: ");
+            Console.ReadLine();
+        }
+
+        private async Task AddFormManually()
+        {
+            Console.Clear();
+            Console.WriteLine("<-Add new FORM->");
+
+            var formDto = new FormDTO();
+            Console.Write("Form Reader ID: ");
+            formDto.ReaderId = int.Parse(Console.ReadLine());
+            Console.Write("Form Book ID: ");
+            formDto.BookId = int.Parse(Console.ReadLine());
+            formDto.BorrowDate = DateTime.UtcNow;
+
+            await this.formService.AddFormAsync(formDto);
+        }
+
+        private async Task AddBook()
+        {
+            var bookDto = new BookDTO();
+
+            Console.Clear();
+            Console.WriteLine("<-Add new READER->");
+
+            Console.Write("Book title: ");
+            var title = Console.ReadLine();
+
+            Console.Write("Book author: ");
+            var author = Console.ReadLine();
+
+            Console.Write("Book year: ");
+            var year = Console.ReadLine();
+
+            var yearIsIntType = Int32.TryParse(year, out var value);
+            if (yearIsIntType)
+            {
+                bookDto.Title = title;
+                bookDto.Author = author;
+                bookDto.Year = value;
+                bookDto.IsAvailable = true;
+
+                await this.bookService.AddBookAsync(bookDto);
+            }
+        }
+
+        private async Task AddReader()
+        {
+            var readerDto = new ReaderDTO();
+
+            Console.Clear();
+            Console.WriteLine("<-Add new READER->");
+
+            Console.Write("Reader fullname: ");
+            var fullName = Console.ReadLine();
+
+            Console.Write("Reader address: ");
+            var address = Console.ReadLine();
+
+            readerDto.Address = address;
+            readerDto.FullName = fullName;
+
+            await this.readerService.AddReaderAsync(readerDto);
+        }
+    }
+}
---
+++ LibraryApp.PL/Program.cs
@@ -0,0 +1,49 @@
+﻿using LibraryApp.BLL;
+using LibraryApp.DAL;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace LibraryApp.PL
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IHost host = CreateHostBuilder(args).Build();
+
+            var scope = host.Services.CreateScope();
+
+            var services = scope.ServiceProvider;
+
+            var applicationTask = services.GetRequiredService<Application>().Start();
+
+            applicationTask.GetAwaiter().GetResult();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] strings)
+        {
+            #region IF_CONSOLE_APP
+            // var configDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            //IConfigurationRoot configuration = new ConfigurationBuilder()
+            //                                       .SetBasePath(configDirectory)
+            //                                       .AddJsonFile("appsettings.json")
+            //                                        .Build();
+            //var connectionString = configuration.GetConnectionString("SqlServerConnectionString");
+
+            #endregion
+
+            #region IF_APPLYING_MIGRATION
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=mylibrary;Trusted_Connection=True;MultipleActiveResultSets=true";
+            #endregion
+
+            return Host.CreateDefaultBuilder()
+                .ConfigureServices((_, services) =>
+                {
+                    services.AddScoped<Application>();
+                    services.AddBusinessLogicLayer();
+                    services.AddDataAccessLayer(connectionString);
+                });
+        }
+    }
+}
